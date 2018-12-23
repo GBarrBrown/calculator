dd44c341f19c21365aa0f3f6e903abc312f3a24c
George-BarrBrown <gbb7899@gmail.com>
2018-12-23 21:48:48 +1300

    Tidy up and comment out console.logs

--- script.js
+++ script.js
@@ -34,82 +34,19 @@ function getVal(val) {
     formulaInput("");
     displayInput("");
   } else if (val === "×" || val === "/" || val === "+" || val === "-") {
-    console.log("operator");
+    // console.log("operator");
     if (temp !== "") {
       entries.push(temp);
       temp = "";
     }
     var lastEntry = entries[entries.length -1];
-    console.log(lastEntry);
+    // console.log(lastEntry);
     if (lastEntry === "×" || lastEntry === "/" || lastEntry === "+" || lastEntry === "-") {
       entries.pop();
     }
     entries.push(val);
     formulaInput(entries);
     displayInput(val);
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  // (val === "×") {
-  //   if (temp !== "") {
-  //     entries.push(temp);
-  //   }
-  //   if (entries[entries.length -1] == "×" || entries[entries.length -1] == "+" || entries[entries.length -1] == "-" || entries[entries.length -1] == "/" ) {
-  //     entries.pop();
-  //     console.log(entries);
-  //   }
-  //   entries.push("×");
-  //   temp = "";
-  //   document.getElementById("formula").value = ""+entries.join("");
-  //   document.getElementById("display").value = "×";
-  // } else if (val === "/") {
-  //   if (temp !== "") {
-  //     entries.push(temp);
-  //   }
-  //   if (entries[entries.length -1] == "×" || entries[entries.length -1] == "+" || entries[entries.length -1] == "-" || entries[entries.length -1] == "/" ) {
-  //     entries.pop();
-  //     console.log(entries);
-  //   }
-  //   entries.push("/");
-  //   temp = "";
-  //   document.getElementById("formula").value = ""+entries.join("");
-  //   document.getElementById("display").value = "/";
-  // } else if (val === "+") {
-  //   if (temp !== "") {
-  //     entries.push(temp);
-  //   }
-  //    if (entries[entries.length -1] == "×" || entries[entries.length -1] == "+" || entries[entries.length -1] == "-" || entries[entries.length -1] == "/" ) {
-  //     entries.pop();
-  //     console.log(entries);
-  //   }
-  //   entries.push("+");
-  //   temp = "";
-  //   document.getElementById("formula").value = ""+entries.join("");
-  //   document.getElementById("display").value = "+";
-  // } else if (val === "-") {
-  //   if (temp !== "") {
-  //     entries.push(temp);
-  //   }
-  //    if (entries[entries.length -1] == "×" || entries[entries.length -1] == "+" || entries[entries.length -1] == "-" || entries[entries.length -1] == "/" ) {
-  //     entries.pop();
-  //     console.log(entries);
-  //   }
-  //   entries.push("-");
-  //   temp = "";
-  //   document.getElementById("formula").value = ""+entries.join("");
-  //   document.getElementById("display").value = "-";
   } else if (val === "=") {
     entries.push(temp);
     temp = "";
@@ -117,7 +54,6 @@ function getVal(val) {
     for (var i = 1; i < entries.length; i++) {
       var nextNum = Number(entries[i+1]);
       var symbol = entries[i];
-
       if (symbol === "+") {
         count += nextNum;
       } else if (symbol === "-") {
@@ -127,22 +63,19 @@ function getVal(val) {
       } else if (symbol === "/") {
         count /= nextNum;
       }
-
       i++;
     }
-    console.log(count);
+    // console.log(count);
     output = count;
-    document.getElementById("display").value = output;
+    displayInput(output);
     entries = [];
   }
   document.getElementById("formula").value = "" + entries.join("") + temp;
-  console.log(temp);
-  console.log(entries);
+  // console.log(temp);
+  // console.log(entries);
 
-  if(temp != "") {
-    document.getElementById("display").value = temp;
-   } // else {
-  //   document.getElementById("display").value = "";
-  // }
-  console.log(output);
+  if(temp !== "") {
+    displayInput(temp);
+   }
+  // console.log(output);
 }
